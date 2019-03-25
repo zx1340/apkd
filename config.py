37349b5fd3bdbe1945f7496a5d26d56366d1c459
zx <phambao@outlook.com>
2019-03-25 15:47:09 +0700

    blacklist

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ host = 'http://127.0.0.1:5000/'
 login_key = b'xxxxx'
 secret_key = 'xxxxxxxxxx'
 apprepo = 'apkdb'
+
+black_list=[
+    'com/google/',
+    'com/crashlytics',
+    'com/facebook',
+]
